164ec14c4943c306b645ba2ecb22e73d445edfe6
Madeleine Kemeny <madeleinekemeny@gmail.com>
2019-09-30 20:24:56 -0500

    fixed text format in details modal

--- client/src/components/VineyardList/index.js
+++ client/src/components/VineyardList/index.js
@@ -1,23 +1,25 @@
 import React from "react";
 
 function VineyardList(props) {
-    return (
-            <div className="card">
-                <div className="card-body">
-                    <div className="row">
-                        <div className="col-sm-12">
-                            <h4>{props.vineyardName}</h4>
-                            <p>{props.subregion} - {props.vineyardRegion}</p>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div>
-                        <p dangerouslySetInnerHTML={{ __html: props.vineyardDetails}}></p>
-                        </div>
-                    </div>
-                </div>   
-            </div>
-    )
+  return (
+    <div className="card">
+      <div className="card-body">
+        <div className="row">
+          <div>
+            <h4>{props.vineyardName}</h4>
+            <p>
+              {props.subregion} - {props.vineyardRegion}
+            </p>
+          </div>
+        </div>
+        <div className="row">
+          <div>
+            <p dangerouslySetInnerHTML={{ __html: props.vineyardDetails }}></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default VineyardList;
